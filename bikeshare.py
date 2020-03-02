77080a29478f87573c60d6dc692d910233da8f92
ashish kishore arya <ashish_kishoreymail.com>
2020-03-02 01:08:18 -0500

    docs: Updated README.md to reflect my Explore Bikeshare Data project

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,8 @@
+#import libraries
 import time
 import pandas as pd
 import numpy as np
-
+#create dict
 CITY_DATA = { 'chicago': 'chicago.csv',
                   'new york city': 'new_york_city.csv',
                   'washington': 'washington.csv' }
